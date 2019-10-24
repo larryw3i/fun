320b71ddca1bb4ae16df4972aefc0bacb0cf5ecb
larry <larryw3i@163.com>
2019-10-24 21:34:13 +0800

    a nice job

--- fun/wearticle/admin.py
+++ fun/wearticle/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from wearticle.models import Wearticle, Comment
+
+@admin.register(Wearticle)
+class WearticleAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'uploaded_time')
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    actions = ['set_like_to_zero', 'set_dislike_to_zero']
+
+    list_display = ('user', 'content', 'like', 'dislike', 'date_time')
+
+    search_fields = ['content', 'user__username']
+
+    def set_like_to_zero(self, request, queryset):
+        rows_updated = queryset.update(like = 0)
+        self.message_user(request, '%s like(s) be set to 0'%rows_updated )
+
+    set_like_to_zero.short_description = 'set like to 0'
+
+
+    def set_dislike_to_zero(self, request, queryset):
+        rows_updated = queryset.update(dislike = 0)
+        self.message_user(request, '%s dislike(s) be set to 0'%rows_updated )
+
+    set_dislike_to_zero.short_description = 'set dislike to 0'
+
+    pass
--- fun/wearticle/models.py
+++ fun/wearticle/models.py
@@ -1,3 +1,38 @@
+
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import User
+import uuid
+
+class Wearticle(models.Model):
+	class Meta:
+		ordering = ['uploaded_time']
+
+	id = models.UUIDField(primary_key=True, auto_created=True, default=uuid.uuid4, editable=False)
+	title = models.CharField( max_length = 64 , unique = True, help_text = 'title')
+	uploaded_time = models.DateTimeField( auto_now_add = True, help_text = 'uploaded time')
+	author = models.ForeignKey( to = User, to_field= 'id', on_delete= models.CASCADE, help_text = 'author')
+	
+	def __str__(self):
+	 	return '%s : %s' % (self.id, self.title[0:20])
+	
+
+class Comment(models.Model):
+	class Meta:
+		ordering = ['date_time']
+	
+	id = models.UUIDField(primary_key=True, auto_created=True, default=uuid.uuid4, editable=False)
+	wearticle = models.ForeignKey(to = Wearticle, to_field= 'id', on_delete = models.CASCADE, help_text = 'article')
+	
+	user = models.ForeignKey( to = User, to_field= 'id', on_delete= models.CASCADE, help_text = 'user')
+
+	content = models.TextField( max_length = 128,  help_text = 'content')
+	like = models.IntegerField( default = 0, help_text = 'like')
+	dislike  = models.IntegerField(default = 0, help_text = 'dislike')
+	date_time = models.DateTimeField( auto_now_add = True, help_text = 'time')
+
+	def __str__(self):
+	 	return '%s : %s' % (self.id, self.content[0:20])
+
