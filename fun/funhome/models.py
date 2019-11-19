4bb2e7c31f5ac44017f4af3801102a5fde0f3be7
larry <larryw3i@163.com>
2019-11-19 18:40:31 +0800

    pdfjs

--- fun/funhome/models.py
+++ fun/funhome/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
+from funfile.storage import upload_to
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Homesticker(models.Model):
+
+    id = models.UUIDField( primary_key=True, default= uuid.uuid4, editable=False, )
+    title = models.CharField( max_length = 64 )
+    cover = models.ImageField( upload_to = upload_to )
+    promulgator = models.ForeignKey(  to = User, on_delete=models.CASCADE, )    
+    content = models.FileField( upload_to = upload_to )
+    promulgating_date = models.DateTimeField(   auto_now_add = True,)
