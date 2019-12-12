adfe8c8b15f9c5c43731d0f0144b2f9128677ca4
larry <larryw3i@163.com>
2019-12-12 12:00:09 +0800

    💡

--- fun/fun/settings.py
+++ fun/fun/settings.py
@@ -212,18 +212,13 @@ LOCALE_PATHS=(
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-] 
+STATIC_ROOT = os.path.join(BASE_DIR,   'staticfiles') 
 
 
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
-
 
 # MEDIA_FILE
 
