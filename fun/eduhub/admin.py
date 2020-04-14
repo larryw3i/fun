697cfe2ad2e5694ae36dba0530b25e6065c4d34b
larry <larryw3i@163.com>
2020-04-14 19:24:51 +0800

    zZ

--- fun/eduhub/admin.py
+++ fun/eduhub/admin.py
@@ -4,36 +4,22 @@ from django.shortcuts import (Http404, HttpResponseRedirect, redirect, render,
                               reverse)
 from django.core.exceptions import ValidationError
 # Register your models here.
-from .models import Content, Label, Funclassification, Eduhubhomesticker
+from .models import Content, Label, Funclassification, Eduhubhomesticker,\
+    Funcontent
 
 from django import forms
 from fun.fundef import default_bleach_clean
 
 
-@admin.register(Content)
-class HomestickerAdmin(admin.ModelAdmin):
-    fields = ['title', 'label', 'content_file', 'comment', 'is_legal', ]
-    list_display = ('title',  'label__name',  'uploading_date', 'is_legal')
-    list_per_page = 10
-
-    ordering = ('-uploading_date',)
-
-    def label__name(self, obj):
-        return obj.label.name
-
 
 @admin.register(Label)
-class HomestickerAdmin(admin.ModelAdmin):
+class LabelAdmin(admin.ModelAdmin):
     fields = ['name', 'comment', 'cover', 'is_legal']
     list_display = ('name', 'author', 'comment',
                     'creating_date', 'is_legal', )
     list_per_page = 10
 
     ordering = ('-creating_date',)
-
-    def save_model(self, request, obj, form, change):
-        obj.author = request.user
-        return super().save_model(request, obj, form, change)
 
 
 @admin.register(Funclassification)
@@ -71,3 +57,20 @@ class EduhubhomestickerAdmin(admin.ModelAdmin):
         obj.content = default_bleach_clean(obj.content)
         obj.promulgator = request.user
         return super().save_model(request, obj, form, change)
+
+
+
+@admin.register( Funcontent )
+class FuncontentAdmin( admin.ModelAdmin ):
+    fields = ['is_legal']
+
+    list_display = ('title', 'label__name', 'content',\
+        'comment','uploading_date' ,\
+        'classification')
+    list_per_page = 10
+
+    ordering = ('-uploading_date',)
+
+    def label__name(self, obj):
+        return obj.label.name
+
