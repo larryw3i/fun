41c8fa1a0d89c2d1dbd7e3bf6724cdc0864a5b65
larryw3i <larryw3i@163.com>
2021-07-17 13:41:43 +0800

    Update fun/funstatic/fun.js: add checkCookies

--- fun/funhome/urls.py
+++ fun/funhome/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path, include
 from . import views
+from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
 
     path('i18n/', include('django.conf.urls.i18n')),
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
     path('homesticker_detail/<uuid:pk>',
          views.HomestickerDetailView.as_view(), name='homesticker_detail'),
     path('homesticker_list', views.HomestickerListView.as_view(),
--- fun/funstatic/fun.js
+++ fun/funstatic/fun.js
@@ -1,9 +1,113 @@
 
 ////// COMMON
 
+function checkCookies(){
+    if( Cookies.get('read_privacy') == undefined ){
+        makeGlobalAlert(
+            `${gettext("Read x-dove.com's Privacy & Cookies")} `+
+            `<button type="button" class="btn btn-info" add-privacy-cookies 
+            data-url='/data_privacy'>${gettext("Read Privacy & Cookies")}&gt;&gt;</button>`,
+            60*60
+        )
+    }
+}
+
+/**
+ * Make a global alert
+ * @param {String} message message
+ * @param {number} timeout in second
+ * @param {String} type Bootstrap alert
+ */
+ function makeGlobalAlert( message='Hello',timeout=2.5, type='info' )
+ {
+     $(`<div class="text-center rounded alert alert-${type}" role="alert">
+         ${message}</div>`)
+         .prependTo('body');
+         
+     setTimeout(()=>{
+         $('.alert').remove();
+     }, timeout*1000);
+ }
+
+/** 
+ * @param { JQuery.ChangeEvent<Document, undefined, any, any> } event 
+ */
+    function refreshLabelList( event )
+    {
+        is_my_label_list = Boolean( Cookies.get('is_my_label_list') );
+        Cookies.set( 
+            'is_my_label_list' , 
+            is_my_label_list?'':'1' , { expires: 365 } );
+        location.reload();
+    }
+
+    /**
+     * 
+     * @param { JQuery.ChangeEvent<Document, undefined, any, any> } event 
+     */
+    function previewVideo(event){
+        var preview_for =  $(`video[preview-for='#${event.target.id}']`) ;
+        if( ! String(event.target.files[0].type).startsWith('video/') ){
+            preview_for.removeAttr('src');
+            preview_for.hide();
+            return;
+        }
+        preview_for.show();
+        preview_for.attr( {
+            'src': URL.createObjectURL( event.target.files[0] )
+        });
+
+    }
+
+
+    /**
+     * 
+     * @param { JQuery.ChangeEvent<Document, undefined, any, any> } event 
+     */
+    function previewPdf(event){
+        var preview_for =$(`object[preview-for='#${event.target.id}']`);
+        if( ! String(event.target.files[0].type).endsWith('/pdf') ) {
+            preview_for.removeAttr('data');
+            preview_for.hide();
+            return;
+        }
+        preview_for.show();
+        preview_for.attr( {
+            'data':'/static/pdf.js/web/viewer.html?file='
+                + URL.createObjectURL( event.target.files[0] ) 
+        });
+
+    }
+
+
+    /**
+     * 
+     * @param { JQuery.ChangeEvent<Document, undefined, any, any> } event 
+     */
+    function changeTheme(event){
+        Cookies.set(
+            'theme', 
+            event.target.dataset.theme,
+            { expires: 365 } ); 
+        location.reload();
+    }
+
+    /**
+     * 
+     * @param { JQuery.ChangeEvent<Document, undefined, any, any> } event 
+     */
+    function previewImage(event){
+        var fileReader = new FileReader();
+        fileReader.onload = function(){
+            $(`img[preview-for='#${event.target.id}']`)
+                .attr( {'src': this.result });
+        }
+        fileReader.readAsDataURL(event.target.files[0])
+    }
 
 (function(){
 
+    checkCookies();
 
     // deprecated
     /////////
@@ -31,11 +135,17 @@
     $(document).on('click', `.language-dropdown-menu .language-dropdown-item` , 
         (event) =>{
         $(`#language_form input[name='language']`)
-            .val(event.target.dataset.language); $(`#language_form`).submit();
+            .val(event.target.dataset.language); $(`#language_form`)
+            .trigger("click");
+    });
+
+    $(document).on('click', `[add-privacy-cookies]` , (event) =>{
+        Cookies.set('read_privacy','1');
+        window.location = event.currentTarget.dataset.url;
     });
 
     $(document).on('click', `[click-to]` , (event) =>{
-        $(`${$(event.currentTarget).attr('click-to')}`).click();
+        $(`${$(event.currentTarget).attr('click-to')}`).trigger('click');
     });
     
 
@@ -52,99 +162,7 @@
     });
 
     if( $('.beian_text').text().trim().length < 1 ) $('.beian_text').remove();
+
     
-    /**
-     * 
-     * @param { JQuery.ChangeEvent<Document, undefined, any, any> } event 
-     */
-    function refreshLabelList( event )
-    {
-        is_my_label_list = Boolean( Cookies.get('is_my_label_list') );
-        Cookies.set( 
-            'is_my_label_list' , 
-            is_my_label_list?'':'1' , { expires: 365 } );
-        location.reload();
-    }
-
-    /**
-     * 
-     * @param { JQuery.ChangeEvent<Document, undefined, any, any> } event 
-     */
-    function previewVideo(event){
-        var preview_for =  $(`video[preview-for='#${event.target.id}']`) ;
-        if( ! String(event.target.files[0].type).startsWith('video/') ){
-            preview_for.removeAttr('src');
-            preview_for.hide();
-            return;
-        }
-        preview_for.show();
-        preview_for.attr( {
-            'src': URL.createObjectURL( event.target.files[0] )
-        });
- 
-    }
-
-
-    /**
-     * 
-     * @param { JQuery.ChangeEvent<Document, undefined, any, any> } event 
-     */
-    function previewPdf(event){
-        var preview_for =$(`object[preview-for='#${event.target.id}']`);
-        if( ! String(event.target.files[0].type).endsWith('/pdf') ) {
-            preview_for.removeAttr('data');
-            preview_for.hide();
-            return;
-        }
-        preview_for.show();
-        preview_for.attr( {
-            'data':'/static/pdf.js/web/viewer.html?file='
-                + URL.createObjectURL( event.target.files[0] ) 
-        });
- 
-    }
- 
-
-    /**
-     * 
-     * @param { JQuery.ChangeEvent<Document, undefined, any, any> } event 
-     */
-    function changeTheme(event){
-        Cookies.set(
-            'theme', 
-            event.target.dataset.theme,
-            { expires: 365 } ); 
-        location.reload();
-    }
-
-    /**
-     * 
-     * @param { JQuery.ChangeEvent<Document, undefined, any, any> } event 
-     */
-    function previewImage(event){
-        var fileReader = new FileReader();
-        fileReader.onload = function(){
-            $(`img[preview-for='#${event.target.id}']`)
-                .attr( {'src': this.result });
-        }
-        fileReader.readAsDataURL(event.target.files[0])
-    }
 
 })();
-
-/**
- * Make a global alert
- * @param {String} message message
- * @param {String} type Bootstrap alert, 
- * view it https://getbootstrap.com/docs/4.4/components/alerts/#examples 
- */
-function makeGlobalAlert( message='Hello',timeout=2500, type='info' )
-{
-    $(`<div class="text-center rounded alert alert-${type}" role="alert">
-        ${message}</div>`)
-        .prependTo('body');
-        
-    setTimeout(()=>{
-        $('.alert').remove();
-    }, timeout);
-}
